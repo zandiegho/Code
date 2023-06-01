e7a597946087b6916670da595669c3e34ad199f6
Zandiegho <jd90099@gmail.com>
2023-05-31 21:39:10 -0500

    Adecuación crud It2

--- php/register.php
+++ php/register.php
@@ -16,14 +16,14 @@ if (isset($_FILES['imagen'])) {
     $temporal = $archivo['tmp_name'];
     
     if (move_uploaded_file($temporal, "../uploads/" . $nombreImagen)) {
-        echo "<br/>La imagen se cargó correctamente.";
+        //echo "<br/>La imagen se cargó correctamente.";
         $rutaImagen = "localhost/code/uploads/$nombreImagen$tipo";
     } else {
         echo "Se produjo un error al cargar la imagen.";
     }
 }
 
-echo "$nombreMarca , $idMarca , $nombreContacto , $apellidoContacto , $telefono , $correo , $descripcion";
+//.0echo "$nombreMarca , $idMarca , $nombreContacto , $apellidoContacto , $telefono , $correo , $descripcion";
 
 
 if(isset($nombreMarca) && isset($idMarca) && isset($nombreContacto) && isset($telefono) && isset($correo)){
@@ -44,11 +44,9 @@ function conex($nameS, $idS, $nameC, $apellidoC, $tel, $mail, $descripcion, $rut
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    }else{
-        echo "<br/>Conexión Exitosa";
     }
 
-    //crear consulta para ingresar datos a tabla User y a tabla negocio
+    //crear consulta para ingresar datos a BD
     $nameCScaped = mysqli_real_escape_string($conn, "$nameC");
     $telCScaped = mysqli_real_escape_string($conn, "$tel");
     $mailCScaped = mysqli_real_escape_string($conn, "$mail");
@@ -78,39 +76,76 @@ function conex($nameS, $idS, $nameC, $apellidoC, $tel, $mail, $descripcion, $rut
         //pasar a Registro Exitoso
 
         // Redirigir a la página "nuevo-destino.php" después de 5 segundos
-        header("refresh:2; url=../Registro-Exitoso.html");
+        //header("refresh:2; url=../Registro-Exitoso.html");
 
         // Mostrar un mensaje antes de redirigir
-        echo "Redireccionando en 5 segundos...";
+        //echo "Redireccionando en 5 segundos...";
 
         // Detener la ejecución del script para que se muestre el mensaje
-        exit;
+        //exit;
+
+
+        ?>
+
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registro Exitoso</title>
+        <!-- Agregamos Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        </head>
+        <body>
+            <div class="div--html"></div>
+                <!-- Espacio del NAV -->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#"> <?php print($nameS) ?> </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Inicio</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Mis Productos</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contacto</a>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </nav>
+
+                <div class="container my-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <h2 class="mb-4">¡Registro exitoso!</h2>
+                                    <p class="lead mb-5">¡Bienvenido/a, <?php printf($nameC .' '. $apellidoC)?> 
+                                    
+                                    </p>
+                                    <a href="../" class="btn btn-primary">Ingresar al sitio de administración</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Agregamos los scripts de Bootstrap -->
+                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+            </div>
+        </body>
+
+        <?php
 
         #########################################################################
 
-        // Conectarse a la base de datos y hacer una consulta
-        
-        $resultado = $conn->query("
-            SELECT u.id_user , u.username, n.id_Bussines, n.name 
-            from user u, negocio n 
-            where u.id_user = n.id_user 
-            and u.id_user = $id_user;
-            ");
-
-        // Convertir el resultado a un arreglo de PHP
-        $datos = array();
-        while ($fila = $resultado->fetch_assoc()) {
-            $datos[] = $fila;
-        }
-
-        header('Content-Type: application/json');
-        echo json_encode($datos);
-
-
-        // Convertir el arreglo a formato JSON y enviarlo al cliente
-
-     }
-
-     
-
+    }
 }
